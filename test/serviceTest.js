2e7b6db17e2c82f70e06e307806bd00d47467e34
Mihai Dima <mihaizn@gmail.com>
2016-10-06 00:42:17 +0300

    separate pattern from service code

--- test/serviceTest.js
+++ test/serviceTest.js
@@ -3,23 +3,22 @@
 const test = require('tap').test
 
 const Mu = require('mu')
-var func = require('mu/drivers/func')
 const Service = require('../service/lib/service')
 
 test('empty answer', (t) => {
   t.plan(2)
-  Service((mu) => {
-    mu.dispatch({role: 's2', cmd: 'one'}, (err, result) => {
+  Service.create((service) => {
+    service.one({}, (err, result) => {
       t.error(err)
-      t.deepEqual({}, result)
+      t.notOk(result)
     })
   })
 })
 
 test('service response', (t) => {
   t.plan(2)
-  Service((mu) => {
-    mu.dispatch({role: 's2', cmd: 'two'}, (err, result) => {
+  Service.create((service) => {
+    service.two({}, (err, result) => {
       t.error(err)
       t.deepEqual({my: 'response'}, result)
     })
