d01b138cf80989ae097e3313c85556c6c9fa183d
Michele Capra <michelecapra83@gmail.com>
2016-10-11 10:55:23 +0200

    fixed linting

--- service/index.js
+++ service/index.js
@@ -1,7 +1,7 @@
 var service = require('./wiring-mu.js')({port: 6000, host: 'localhost'})
 
 service.start(
-  function(){
-    console.log('service started');
+  function () {
+    console.log('service started')
   }
-);
+)
