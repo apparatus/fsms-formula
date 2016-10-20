6deae6befdb830528d0b3a25dafbbe850a8cee6d
davidmarkclements <huperekchuno@googlemail.com>
2016-10-20 21:03:28 +0100

    rm non req bits

--- integration/service.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var opts = {port: process.env.SERVICE_PORT, host: process.env.SERVICE_HOST}
-const wiring = require('../service/wiring-mu')(opts)
-
-wiring.start(() => {
-  console.log(`service listening on port: ${opts.port}`)
-})
