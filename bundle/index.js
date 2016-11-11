86edd1503ce492babd0cd0821c52fc87e600b3e1
davidmarkclements <huperekchuno@googlemail.com>
2016-11-12 00:24:47 +0100

    bundle can return stream (for build)

--- bundle/index.js
+++ bundle/index.js
@@ -7,33 +7,42 @@ module.exports = bundle
 function bundle (opts) {
   var cached = ''
 
-  make((err, code) => {
-    if (err) {
-      console.warn('Unable to create initial bundle', err)
-      return
-    }
-    cached = code
-  })
-
-  return html
-
-  function html (cb) {
-    if (cached) return cb(null, cached)
+  if (opts.dev) {
     make((err, code) => {
-      if (err) return cb(err)
+      if (err) {
+        console.warn('Unable to create initial bundle', err)
+        return
+      }
       cached = code
-      cb(null, code)
     })
   }
 
+  return cmp
+
+  function cmp (cb) {
+    if (cached) return cb(null, cached)
+    return make(cb && ((err, code) => {
+      if (err) return cb(err)
+      if (opts.dev) cached = code
+      cb(null, code)
+    }))
+  }
+
   function make (cb) {
-    browserify({builtins: builtins, standalone: opts.dev && name})
-      .add(require.resolve('../cmp'))
-      .bundle((err, buf) => {
-        if (err) return cb(err)
-        buf = buf.toString()
-        cb(null, buf)
-      })
+    var build = browserify(Object.assign(
+      {builtins: builtins, standalone: opts.dev && name},
+      opts
+    ))
+
+    build.add(require.resolve('../cmp'))
+
+    if (!cb) return build.bundle()
+
+    build.bundle((err, buf) => {
+      if (err) return cb(err)
+      buf = buf.toString()
+      cb(null, buf)
+    })
   }
 
 }
